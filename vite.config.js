d033781d8a0dc356235bf20340f7f4a500617e02
koechy77 <kingsleyonyem7@gmail.com>
2026-06-05 10:58:19 +0100

    changed the base to a new repo

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwind from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/React-portfolio/',
+  base: '/Portfolio/',
   plugins: [react(), tailwind()],
 })
